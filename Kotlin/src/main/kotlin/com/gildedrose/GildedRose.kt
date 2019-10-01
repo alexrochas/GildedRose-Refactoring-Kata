9497d38041a6a8adde0be586c2cf5e8077e15794
Alex Rocha <arocha@zalando-61672.corp.ad.zalando.net>
2019-10-01 17:07:24 +0200

    feat: trivial solution for new feature

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -6,7 +6,9 @@ class GildedRose(var items: Array<Item>) {
         for (i in items.indices) {
             if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (items[i].name.startsWith("Conjured")) {
+                        items[i].quality = items[i].quality - 2
+                    } else if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1
                     }
                 }
