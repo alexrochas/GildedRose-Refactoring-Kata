95e904df33142435bab1eafcc9e514c3ed907d0b
Alex Rocha <arocha@zalando-61672.fritz.box>
2019-10-05 23:37:28 +0200

    refactor: solution using comp

--- clojure/src/gildedrose_refactoring_kata/core.clj
+++ clojure/src/gildedrose_refactoring_kata/core.clj
@@ -6,35 +6,33 @@
   "update quality in list of items"
   [items]
   (map #(
-    do (
+    (comp 
       (if (and (not (= (:name %) "Aged Brie")) (not (= (:name %) "Backstage passes to a TAFKAL80ETC concert"))) (
-            if (> (:quality %) 0) (
-              if (not (= (:name %) "Sulfuras, Hand of Ragnaros")) (update % :quality dec)
-            )
-          ) (
-            if (< (:quality %) 50) (
-              do (
-                (update % :quality inc)
-                (if (= (:name %) "Backstage passes to a TAFKAL80ETC concert") (
-                  do (
-                    (if (< (:sell_in %) 11) 
-                      (if (< (:quality %) 50) (update % :quality inc)))
-                    (if (< (:sell_in %) 6) 
-                      (if (< (:quality %) 50) (update % :quality inc)))
-                  )
-                ))
-              )
-            )
-          )
+                  if (> (:quality %) 0) (
+                    if (not (= (:name %) "Sulfuras, Hand of Ragnaros")) (update % :quality dec) (%)
+                  ) (%)
+                ) (
+                  if (< (:quality %) 50) (
+                    do (
+                      (update % :quality inc)
+                      (if (= (:name %) "Backstage passes to a TAFKAL80ETC concert") (
+                        do (
+                          (if (< (:sell_in %) 11) 
+                            (if (< (:quality %) 50) (update % :quality inc) %) %)
+                          (if (< (:sell_in %) 6) 
+                            (if (< (:quality %) 50) (update % :quality inc) %) %)
+                        )
+                      ))
+                    )
+                  ) (%)
+                )
       )
-      (if (not (= (:name %) "Sulfuras, Hand of Ragnaros")) (update % :sell_in dec))
+      (if (not (= (:name %) "Sulfuras, Hand of Ragnaros")) (update % :sell_in inc) %)
       (if (< (:sell_in %) 0) (
         if (and (not (= (:name %) "Aged Brie")) (not (= (:name %) "Backstage passes to a TAFKAL80ETC concert"))) (
           if (> (:quality %) 0) (
-            if (not (= (:name %) "Sulfuras, Hand of Ragnaros")) (update % :quality dec)
-          )
-        ) (if (< (:quality %) 50) (update % :quality inc)))
-      ))
-    )
-  items)
-)
+            if (not (= (:name %) "Sulfuras, Hand of Ragnaros")) (update % :quality dec) %
+          ) (%)
+        ) (if (< (:quality %) 50) (update % :quality inc) (%))) %
+      )
+    ) %))items)
